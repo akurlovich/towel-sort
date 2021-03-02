a65b2bcb5eb2a351c8a8a2f8fe2236dc437bb81d
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-02 09:02:18 +0300

    feat: function complite

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 
 // You should implement your task here.
 
-// module.exports = 
-function towelSort (matrix) {
+module.exports = function towelSort (matrix) {
   let newArr = [];
   if (!(matrix instanceof Array)) {
     return [];
@@ -11,16 +10,23 @@ function towelSort (matrix) {
   } else {
     for (let i = 0; i < matrix.length; i++) {
       // console.log(matrix.length);
-      if (i == 1) {
+      if (i % 2 !== 0) {
         matrix[i] = matrix[i].reverse();
       }
-    console.log(matrix[i]);
+    // console.log(matrix[i]);
     }
   }
+  newArr = matrix.flat();
+  return newArr;
 }
 // console.log(towelSort[[1, 2], [3, 4]]);
-const matrix = [[1, 2], [3, 4]];
+// const matrix = [
+//   [1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 10, 11, 12],
+//   [13, 14, 15, 16],
+// ];
 // let nArr = matrix.flat();
-// console.log(nArr);
+// console.log(matrix);
 
-towelSort(matrix);
+// towelSort(matrix);
